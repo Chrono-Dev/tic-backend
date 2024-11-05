57e1fb1f0e1766fd5d4be0223a97fac1f6f0257e
Isaac Vinícius <igvinilink7@gmail.com>
2024-11-05 19:39:10 -0300

    Altera Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,9 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://chrono-dev.github.io/'
-      resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
-    end
+      origins 'https://chrono-dev.github.io'
+      resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options]
+   end
   end
