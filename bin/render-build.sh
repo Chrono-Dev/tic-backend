6febffa1543eab5d7cd633a3c658bc7af792a71c
Isaac Vinícius <igvinilink7@gmail.com>
2024-11-05 01:02:36 -0300

    TESTE

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Instala as dependências
 bundle install
 
-# Realiza as migrações do banco de dados
-bundle exec rails db:migrate RAILS_ENV=production
-
-# Inicia o servidor
-bundle exec puma -t 5:5 -p ${PORT:-3000} -e production
+bundle exec rake db:create
+bundle exec rake db:schema:load
+bundle exec rake db:schema:load
