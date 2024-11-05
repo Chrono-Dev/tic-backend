5ca23f3efa7f1c7b349acbb4964215c052995f6c
Isaac Vinícius <igvinilink7@gmail.com>
2024-11-04 23:09:23 -0300

    Adiciona Rails DB:Migrate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 bundle install
+rails db:migrate
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
